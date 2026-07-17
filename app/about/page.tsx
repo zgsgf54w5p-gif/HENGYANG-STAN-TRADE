3b034bf6841cc549ae1d474bca99698245828b9c
zgsgf54w5p-gif <zgsgf54w5p@privaterelay.appleid.com>
2026-07-17 10:09:55 +0800

    Fix empty about page

---
+++ app/about/page.tsx
@@ -0,0 +1,32 @@
+export default function AboutPage() {
+  return (
+    <main
+      style={{
+        padding: "80px 8%",
+        minHeight: "100vh",
+        background: "#F5F8FC",
+      }}
+    >
+      <h1
+        style={{
+          color: "#0B4EA2",
+          fontSize: "48px",
+          textAlign: "center",
+        }}
+      >
+        About Us
+      </h1>
+
+      <p
+        style={{
+          textAlign: "center",
+          marginTop: "20px",
+          color: "#555",
+          fontSize: "18px",
+        }}
+      >
+        Welcome to Hengyang Stan Trade Development Co., Ltd. We specialize in supplying high-quality home appliances and kitchen equipment to customers worldwide.
+      </p >
+    </main>
+  );
+}

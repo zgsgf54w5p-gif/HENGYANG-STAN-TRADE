91d7fb46a9c5a0c49c16070a1e935705a20e0685
zgsgf54w5p-gif <zgsgf54w5p@privaterelay.appleid.com>
2026-07-17 09:36:20 +0800

    Update page.tsx

--- app/about/testimonials/page.tsx
+++ app/about/testimonials/page.tsx
@@ -1,33 +1,8 @@
 export default function TestimonialsPage() {
   return (
-    <main
-      style={{
-        padding: "80px 8%",
-        minHeight: "100vh",
-        background: "#F5F8FC",
-      }}
-    >
-      <h1
-        style={{
-          color: "#0B4EA2",
-          fontSize: "48px",
-          textAlign: "center",
-        }}
-      >
-        Customer Testimonials
-      </h1>
-
-      <p
-        style={{
-          textAlign: "center",
-          marginTop: "20px",
-          color: "#555",
-          fontSize: "18px",
-        }}
-      >
-        This page is coming soon. Our customer reviews and success stories will
-        be available here.
-      </p >
+    <main>
+      <h1>Customer Testimonials</h1>
+      <p>Coming soon...</p >
     </main>
   );
 }
